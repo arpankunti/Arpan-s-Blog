e4c3f083f22818053ee022eabf806979a5d9ae0b
arpankunti <95353995+arpankunti@users.noreply.github.com>
2022-02-08 10:54:19 +0530

    Add files via upload

---
+++ Sankey Project/MyApplication/src/main/java/com/arpan/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.arpan.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.arpan.modal.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+	public User findByUsernameAndPassword(String username, String password);
+}
